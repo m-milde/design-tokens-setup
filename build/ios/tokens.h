4734d691e8eccfb1f10c808b150cebd518dbe679
m-milde <m.milde@tinqin.com>
2024-12-11 18:54:13 +0200

    Add convertor for rem to px

--- build/ios/tokens.h
+++ build/ios/tokens.h
@@ -9,6 +9,23 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#define SpaceRootSpacer 240.00f
-#define SpaceSemanticGap 240.00f
-#define SpaceComponentButtonGap 240.00f
+#define SpaceRootSpacer 160.00f
+#define SpaceSemanticGap 160.00f
+#define SpaceComponentButtonGap 160.00f
+#define RootFontSizeFontSize1 192.00f
+#define RootFontSizeFontSize2 224.00f
+#define RootFontSizeFontSize3 240.00f
+#define RootFontSizeFontSize4 256.00f
+#define RootFontSizeFontSize5 288.00f
+#define RootFontSizeFontSize6 320.00f
+#define RootFontSizeFontSize7 384.00f
+#define RootFontSizeFontSize8 480.00f
+#define AliasFontSizeFontSize12 192.00f
+#define AliasFontSizeFontSize14 224.00f
+#define AliasFontSizeFontSize15 240.00f
+#define AliasFontSizeFontSize16 256.00f
+#define AliasFontSizeFontSize18 288.00f
+#define AliasFontSizeFontSize20 320.00f
+#define AliasFontSizeFontSize24 384.00f
+#define AliasFontSizeFontSize30 480.00f
+#define CardMainTextFontSize 256.00f
