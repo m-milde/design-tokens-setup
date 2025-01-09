2b4db195a49c000b605f9987dbdc90180a1432a4
m-milde <m.milde@tinqin.com>
2025-01-09 18:29:10 +0200

    Add Spacing tokens to SOT folder

---
+++ Sources/Tokens/SapphireColors.swift
@@ -0,0 +1,18 @@
+
+//
+// SapphireColors.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import SwiftUI
+
+public struct SapphireColors {
+    public static let brandPrimary100 = Color(red: 0.337, green: 0.220, blue: 0.898, opacity: 1)
+    public static let brandPrimary50 = Color(red: 0.667, green: 0.608, blue: 0.949, opacity: 1)
+    public static let brandPrimary10 = Color(red: 0.902, green: 0.882, blue: 0.984, opacity: 1)
+    public static let accentBrandDefault = brandPrimary100
+    public static let accentBrandLight = brandPrimary50
+    public static let accentBrandLighter = brandPrimary10
+}
